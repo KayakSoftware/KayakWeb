8f5986e47a7e353f44218ed0232d318bb8a816e4
Ulrik Sandberg <UJSandberg@gmail.com>
2020-10-18 18:31:00 +0200

    call predictor test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 
 function App() {
-  return <div>Hejsa jeg er en app</div>;
+
+  const [prediction, setPrediction] = useState("")
+
+  const uploadData = async () => {
+
+   var result = await axios.post("http://localhost:5000/predict",
+   {
+     data: [{
+       timeStamp: 123123,
+       xAxis: 10,
+       yAxis: 15,
+       zAxis: 11
+     },
+     {
+      timeStamp: 123123,
+      xAxis: 10,
+      yAxis: 15,
+      zAxis: 11
+    }]
+   })
+
+   setPrediction(result.data);
+
+   console.log(result)
+  }
+
+  return <div>Hejsa jeg er en app
+    <button onClick={() => uploadData()}>Uploaad</button>
+    Prediction: {prediction}
+  </div>;
 }
 
 export default App;
